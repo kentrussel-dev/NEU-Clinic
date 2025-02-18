96338a4a2416a96a56c2c7aabed8ddaae8c557d0
kentrussel <krb@egcextremeunrealtechnology.com>
2025-02-19 01:23:19 +0800

    initial

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Data
+{
+    public class AppDbContext : IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebApp.Models
+{
+    public class Users : IdentityUser
+    {
+        public string FullName { get; set; }
+
+
+    }
+}
