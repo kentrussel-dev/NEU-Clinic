f766bdee6a831155caea24dfca079dfd9eee5e43
kentrussel <krb@egcextremeunrealtechnology.com>
2025-02-25 17:59:54 +0800

    last update for account profile

--- obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2f7702e7598dade1e7a88de892c424a445bc72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c800425dc12478ece11aadd8d773cb0adc099ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
