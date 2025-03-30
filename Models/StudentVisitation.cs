adb48aed532857518ec92076f214cc5f58dbce45
Kent Russel Balajadia <kentggrussel@gmail.com>
2025-03-30 17:13:38 +0800

    fix

---
+++ Models/StudentVisitation.cs
@@ -0,0 +1,19 @@
+﻿// Models/StudentVisitation.cs
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
+{
+    public class StudentVisitation
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public Users User { get; set; }
+        public string Purpose { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime VisitationDate { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
